c66cd668c1efc4824db6a6576e6ba6599725afb3
Yabruk <sahkorachko@gmail.com>
2024-12-10 23:26:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -54,5 +54,5 @@ def set_webhook() -> str:
         return "Помилка встановлення вебхука", 500
 
 if __name__ == '__main__':
-    # Запускаємо Flask-додаток
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
